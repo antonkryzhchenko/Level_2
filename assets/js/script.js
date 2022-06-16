9ce8b9bff42af190e5e74e5aac2a4542c35bd422
Anton Kryzhchenko <a.p.kryzhchenko@gmail.com>
2022-06-16 21:35:45 +0300

    init repo

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+// header
+let header = document.querySelector(".header");
+
+document.addEventListener("scroll", () => {
+  if (document.documentElement.scrollTop > 1) {
+    header.classList.add("header_has-bg");
+  } else {
+    header.classList.remove("header_has-bg");
+  }
+});
+
+// Animation
+  AOS.init();
